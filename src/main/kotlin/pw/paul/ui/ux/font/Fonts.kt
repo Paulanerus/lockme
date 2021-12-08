e6bedb4d83e2a950a1c4e27fcb7e3ebde72ff187
Paul <reallucycode@gmail.com>
2021-12-08 19:07:15 +0100

    Updated LockMeTheme

--- src/main/kotlin/pw/paul/ui/ux/font/Fonts.kt
+++ src/main/kotlin/pw/paul/ui/ux/font/Fonts.kt
@@ -1,12 +1,15 @@
 package pw.paul.ui.ux.font
 
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 
+val InterTypography = Typography(defaultFontFamily = interLightFontFamily())
+
 @Composable
-fun interLightFontFamily() = FontFamily(
+private fun interLightFontFamily() = FontFamily(
     Font("assets/ux/font/Inter-Light.ttf", FontWeight.Light, FontStyle.Normal)
 )
--- src/main/kotlin/pw/paul/ui/ux/theme/LockMeTheme.kt
+++ src/main/kotlin/pw/paul/ui/ux/theme/LockMeTheme.kt
@@ -1,14 +1,14 @@
-package pw.paul.ui.ux.color
+package pw.paul.ui.ux.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import pw.paul.ui.ux.font.InterTypography
 
-val darkColorScheme = darkColors(
-    primary = Color(0xFF202731),
+val darkColorScheme = darkColors(primary = Color(0xFF202731),
     primaryVariant = Color(0xFF333F4D),
     secondary = Color.White,
     secondaryVariant = Color(0xFFBBBFC4),
@@ -19,12 +19,18 @@ val darkColorScheme = darkColors(
     onSecondary = Color.White,
     onBackground = Color.White,
     onSurface = Color.White,
-    onError = Color.White
-)
+    onError = Color.White)
 
 val lightColorScheme = lightColors()
 
 @Composable
-fun appColors(): Colors {
-    return if (isSystemInDarkTheme()) darkColorScheme else lightColorScheme
+fun LockMeTheme(content: @Composable () -> Unit) {
+
+    val colors = if (isSystemInDarkTheme()) {
+        darkColorScheme
+    } else {
+        lightColorScheme
+    }
+
+    MaterialTheme(colors = colors, typography = InterTypography, content = content)
 }
